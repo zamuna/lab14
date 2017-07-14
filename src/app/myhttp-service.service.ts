acd58f7cb7a6eeeb137d68596d8d5adece0f76c5
zamuna <zamuna16@gmail.com>
2017-07-13 22:00:47 -0500

    http to https

--- src/app/myhttp-service.service.ts
+++ src/app/myhttp-service.service.ts
@@ -1,13 +1,17 @@
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
-import { Observable } from 'rxjs/Observable'
+import { Http } from '@angular/http'; 
+import { Observable } from 'rxjs/Rx'
 import 'rxjs/add/operator/map'
 
 @Injectable()
 export class MyhttpServiceService {
 
-  constructor(private http:Http) { }
-getMyData(url:string){
-  return this.http.get(url);
+  constructor(private http: Http) { }
+  getMyData() {
+    let userResponse = this.http.get('https://jsonplaceholder.typicode.com/users/1').map(res => res.json());
+    let postsResponse = this.http.get('https://jsonplaceholder.typicode.com/posts?userId=1').map(res => res.json());
+
+    return Observable.forkJoin([userResponse, postsResponse]);
+  }
 }
-}
+
